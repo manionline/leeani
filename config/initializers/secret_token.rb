735d2b59e8482559c2e28d4d3b308ae8437818b9
manionline <manionline@yahoo.com>
2013-06-05 13:56:28 -0400

    Initial commit for Leeani

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Leeani::Application.config.secret_key_base = '9ecb5ef2e0ad2956ba0ef36e2dba2b189c146c28da58fa8f6b1fad6f4d64d96a213e0ab605725b2dcba7fc69944af43e2d8a70a5945ab29c34b16d4038706184'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Leeani::Application.config.session_store :cookie_store, key: '_leeani_session'
