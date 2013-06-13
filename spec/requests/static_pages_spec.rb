8a823bead5d61115b3635d4ad5f3da8d8bf8cbe1
manionline <manionline@yahoo.com>
2013-06-13 16:45:48 -0400

    Fininsh Static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "GET /static_pages" do
+
+    it "works! (now write some real specs)" do
+      puts "Inside"
+      end
+    end
+  end
+end
